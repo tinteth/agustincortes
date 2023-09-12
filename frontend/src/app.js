abf41b1c491220ce59b8767cb00e6d3714f4d3f4
tinteth <121214172+tinteth@users.noreply.github.com>
2023-09-12 12:34:36 -0700

    Initial commit

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="center-text">
+        Agustin Cortes
+      </div>
+    </div>
+  );
+}
+
+export default App;
