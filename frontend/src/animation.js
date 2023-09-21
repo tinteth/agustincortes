dbd2ce534f508d65009f2c79fe6cb8129cd4da99
Agustin Cortes Hernandez <121214172+tinteth@users.noreply.github.com>
2023-09-20 17:25:08 -0700

    Create animation.js

---
+++ frontend/src/animation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Animation.css';
+
+const Animation = () => {
+  return (
+    <div className="animation-container">
+      <div className="box box1"></div>
+      <div className="box box2"></div>
+      <div className="box box3"></div>
+      <div className="box box4"></div>
+    </div>
+  );
+};
+
+export default Animation;
